49599cfe6a9c9126ae11dd25d89660234b80e39c
guotan1220 <1796926725@qq.com>
2023-10-24 16:37:49 +0800

    日志使用学习

--- duoduoshop/duoduoshop/urls.py
+++ duoduoshop/duoduoshop/urls.py
@@ -16,7 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
+def log(request):
+    # 1.导入loging
+    import logging
+    # 2.创建日志
+    logger = logging.getLogger('django')
+    # 3.调用日志器的方法来保存日志
+    logger.info('用户登陆了')
+    logger.warning('redis缓存不足')
+    logger.error('该记录不存在')
+    logger.debug('~~~~~~~~~~~~')
+    return HttpResponse('log')
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('log/', log)
 ]
